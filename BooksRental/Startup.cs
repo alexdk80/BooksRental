ffc41c5b1c7a8e75c088faa4761639bbb5caf580
Oleksandr Shymon <alexdk80@gmail.com>
2017-12-27 17:54:52 +0100

    Add project files.

---
+++ BooksRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BooksRental.Startup))]
+namespace BooksRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
