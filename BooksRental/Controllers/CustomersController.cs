71c2948844cc3e69b9e721681073194eda8ddb06
Oleksandr Shymon <alexdk80@gmail.com>
2017-12-28 20:20:10 +0100

    Changes in Customer, MembershipType, Index_Customer CustomersController

--- BooksRental/Controllers/CustomersController.cs
+++ BooksRental/Controllers/CustomersController.cs
@@ -5,11 +5,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace BooksRental.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
@@ -23,32 +34,27 @@ namespace BooksRental.Controllers
             //    Customers = customers
             //};
             //return View(viewModel);
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
         public ActionResult Details(int id)
         {
-            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
-            //return View(customer);
-            Customer c = new Customer();
-            foreach (Customer item in GetCustomers())
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer==null)
             {
-                if (item.Id == id)
-                {
-                    c = item;
-                }
+                return HttpNotFound();
             }
+            return View(customer);
+            //Customer c = new Customer();
+            //foreach (Customer item in GetCustomers())
+            //{
+            //    if (item.Id == id)
+            //    {
+            //        c = item;
+            //    }
+            //}
 
-            return View(c);
-        }
-        public IEnumerable<Customer> GetCustomers()
-        {
-            var customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Doe" },
-                new Customer { Id = 2, Name = "Jane Doe" }
-            };
-            return customers;
-        }
+            //return View(c);
+        }        
     }
 }
